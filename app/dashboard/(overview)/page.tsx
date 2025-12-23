6e4f35a4ca41c08cbb645b7a7842f4c61c2fc463
Zohaib Khan | Front-End Web Developer <zk3165657513@gmail.com>
2025-12-23 17:56:31 +0500

    Cmpleated dashboard project using nextjs

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,62 @@
+
+import  RevenueChart from '@/app/ui/dashboard/revenue-chart'
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices'
+import { Main } from 'next/document';
+import { LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+import { Card} from '@/app/ui/dashboard/cards'
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import CardWrapper from '@/app/ui/dashboard/cards';
+import { CardSkeleton } from '@/app/ui/skeletons';
+export default async function Page(){
+   
+   
+   
+      //const {      numberOfCustomers,
+    //  numberOfInvoices,
+    //  totalPaidInvoices,
+    //  totalPendingInvoices,
+    //}= await fetchCardData()
+
+
+   return(
+
+      <>
+      <main> <h1 className='mb-8'>Dashboard</h1>
+
+
+  < div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+ 
+<Suspense fallback={<CardSkeleton/>}>
+   <CardWrapper/>
+</Suspense>
+ 
+
+       {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />/*/}
+      </div>
+      
+
+<div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'> 
+   
+     <Suspense 
+     fallback={<RevenueChartSkeleton/>}>
+     <RevenueChart  />
+     </Suspense>
+     <Suspense fallback={<LatestInvoicesSkeleton/>}>
+   <LatestInvoices/>
+
+     </Suspense>
+</div>
+
+   </main>
+   </>
+  
+   )
+}
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,12 +16,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+     console.log('Fetching revenue data...');
+     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+     console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -42,6 +42,7 @@ export async function fetchLatestInvoices() {
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
+      
     }));
     return latestInvoices;
   } catch (error) {
@@ -156,9 +157,11 @@ export async function fetchInvoiceById(id: string) {
 
     const invoice = data.map((invoice) => ({
       ...invoice,
+      
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
+    console.log(invoice); // Invoice is an empty array []
 
     return invoice[0];
   } catch (error) {
